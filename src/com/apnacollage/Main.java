b63cb1223564b31c21418df4eb40a01bfc270e5b
DELL <mmomi2410013@bscse.uiu.ac.bd>
2025-10-07 10:16:38 +0600

    Initial commit

---
+++ src/com/apnacollage/Main.java
@@ -0,0 +1,14 @@
+package com.apnacollage;
+
+public class Main {
+    public static void main(String[] args) {
+        //next line print
+        System.out.println("Hello World with Java");
+        System.out.println("Hello World with Java");
+        //backslash n using
+        System.out.print("Hello World with Java\n");
+        System.out.println("Hello World with Java\n");
+        //output finding
+        System.out.println("Hello World with java\nfrom Apna Collage");
+    }
+}
---
+++ src/com/apnacollage/pattarn.java
@@ -0,0 +1,37 @@
+package com.apnacollage;
+
+import java.sql.SQLOutput;
+
+public class pattarn {
+    public static void main(String[] args) {
+        //pattarn printing
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+                                      //Data Types
+        //1. Primitive Data type
+        // char = 2 byte
+        //boolean = 1 byte
+        //int = 4 byte
+        //long = 8 byte
+        //float=4 byte                    //2.Non-primitive Data type
+        //double = 8 byte                 //String
+                                          //Array
+                                          //Class
+                                          // Object
+                                          //Interface
+        //variables
+        //output
+        int a =10;
+        int b=18;
+        int sum=a+b;
+        System.out.println(sum);
+        int diff = b-a;
+        System.out.println(diff);
+        int mul=a*b;
+        System.out.println(mul);
+
+    }
+}
