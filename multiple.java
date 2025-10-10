6f59ae307de28c1da1d5add21449594385823b66
Mahmuda Akter Momi <mmomi2410013@bscse.uiu.ac.bd>
2025-10-10 16:17:38 +0600

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class multiple {
+    public static int calculateProduct(int a,int b){
+        return a*b;
+    }
+
+    public static void main(String[] args)
+        {
+        Scanner sc=new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int product=calculateProduct(a,b);
+        System.out.println(product);
+
+    }
+}
